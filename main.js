68b74827acd4757c5d78448e65b9d902e6f6f420
Interact-fan <95719545+Interact-fan@users.noreply.github.com>
2021-12-07 16:48:10 -0500

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-   $('#banana').rotate({maxAngle:25,minAngle:-55,
-      bind: [
-        {"mouseover":function(){$(this).rotateAnimation(85); } },
-        {"mouseout":function(){$(this).rotateAnimation(85); } }
-      ]
-   });
-});
