d99d5ce199a478e20b5f49e490885cc047067b75
Interact-fan <95719545+Interact-fan@users.noreply.github.com>
2021-12-26 10:03:36 -0500

    Update main.js

--- html/main.js
+++ html/main.js
@@ -1,1 +1,13 @@
-
+var c = document.getElementById("canvas1");
+var ctx = c.getContext("2d");
+// uses id (canvas1)
+var grd = ctx.createLinearGradient(0,0,200,0);
+// note: can have more color stops
+// stop 1
+grd.addColorStop(0,"Silver");
+// stop 2
+grd.addColorStop(1,"gray");
+// continue stops here;
+ctx.fillStyle = grd;
+// how big the canvas is
+ctx.fillRect(0,0,2000,2000);
